2a84e1fb06c98780b4f4282228589d82363e0909
Moshood Alaran <malaran@terragonltd.com>
2019-04-01 20:41:43 +0100

    What ever changes

--- routes/web.php
+++ routes/web.php
@@ -77,6 +77,6 @@ Route::group(['prefix' => 'admin/'], function() {
     });
 
     Route::get('home', function(){
-        return view('admin.blank');
+        return view('admin.index');
     });
 });
