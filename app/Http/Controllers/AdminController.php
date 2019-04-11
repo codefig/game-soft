a4c8fec9ead0a440a60a2a2d10c8e20d9a03abd6
Moshood Alaran <malaran@terragonltd.com>
2019-04-11 01:27:40 +0100

    added except keyword to iddleware

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,10 +7,14 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function __construct(){
-        $this->middleware('guest:admin');
+        $this->middleware('guest:admin')->except('logout');
     }
 
     public function showLogin(){
         return "This returns the login form view ";
+    }
+
+    public function postLoginForm(Request $request){
+        return "this is the post login form for admin";
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,4 +7,18 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     //
+
+    public function __construct()
+    {
+        $this-middleware('guest:user')->except('logout');
+    }
+
+    public function showLoginForm(){
+
+        return "this is the user login form ";
+    }
+
+    public function postLoginForm(Request $request){
+        return "this is the login form submission";
+    }
 }
