165319ab0fc4683878b3a87f0c4b12932ea6b226
Moshood Alaran <malaran@terragonltd.com>
2019-04-11 01:17:19 +0100

    made model changes to admin model

--- app/Admin.php
+++ app/Admin.php
@@ -2,17 +2,32 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
 
     protected $guard = 'admin';
 
-    protected $filable = ['email', 'password'];
+    protected $fillable = [
+        'email', 'password',
+    ];
 
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
     protected $hidden = [
-        'password'
+        'password',
     ];
 }
