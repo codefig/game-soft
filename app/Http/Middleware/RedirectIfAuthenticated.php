c06b02cb7b0683bb4fec3b04ead44a8ba60795cb
Moshood Alaran <malaran@terragonltd.com>
2019-04-11 02:26:06 +0100

    added perfect redirection url for admin and normal user

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,9 +18,11 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
+            if($guard == 'admin'){
+                return redirect("/admin/home");
+            }
             return redirect('/home');
         }
-
         return $next($request);
     }
 }
