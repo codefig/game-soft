77c170785a2e411a7cef45824d2a6d1898a266ee
Moshood Alaran <geekyshood@gmail.com>
2019-04-13 06:24:48 +0100

    added vciew for showing all sessions

--- resources/views/admin/showsessions.blade.php
+++ resources/views/admin/showsessions.blade.php
@@ -162,9 +162,48 @@
         </ol>
 
         <!-- Page Content -->
-        <h1>Blank Page</h1>
+        <h1>All Sessions</h1>
         <hr>
         <p>This is a great starting point for new custom pages.</p>
+
+
+        <div class="container bodycontainer">
+            <table class="table table-dark">
+                <thead>
+                  <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Duration</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Actions</th>
+
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
+        </div>
 
       </div>
       <!-- /.container-fluid -->
