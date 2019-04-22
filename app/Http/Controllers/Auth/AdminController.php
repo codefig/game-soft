7ad8eecf6421cd34cb256d4a1b57c200c3b0aad7
Moshood Alaran <geekyshood@gmail.com>
2019-04-22 10:37:48 +0100

    Category delete and error validation now working

--- app/Http/Controllers/Auth/AdminController.php
+++ app/Http/Controllers/Auth/AdminController.php
@@ -26,7 +26,7 @@ class AdminController extends Controller
 
     public function logout(){
 
-        Auth::logout();
+        Auth::guard('admin')->logout();
         return redirect()->route('admin.login');
     }
 
