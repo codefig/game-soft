cca0164201e8cd8e95a5e5e946118f780a3f010c
Moshood Alaran <geekyshood@gmail.com>
2019-04-29 00:50:27 +0100

    about to add the pass_value column to stages

--- database/migrations/2019_04_24_010245_create_stages_table.php
+++ database/migrations/2019_04_24_010245_create_stages_table.php
@@ -17,6 +17,7 @@ class CreateStagesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('timeframe');
+            $table->integer('pass_value');
             $table->integer('lap_id');
             $table->integer('category_id');
             $table->timestamps();
