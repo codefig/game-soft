26fb9f6e9b5b35fe32ad6451c3dca9550b8e09b7
Moshood Alaran <geekyshood@gmail.com>
2019-05-07 10:30:09 +0100

    fed up with current line

---
+++ app/Http/Controllers/VerificationContoller.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class VerificationController extends Controller
+{
+
+
+    public function verifyEmail($token){
+        $user = User::where('token', $token)->firstOrFail();
+
+        $user->update(['token'=>null]);
+
+        return redirect()->route('home')->with('success', "Account verification successfull");
+    }
+}
---
+++ resources/views/vendor/mail/markdown/index.blade.php
@@ -0,0 +1,41 @@
+@component('mail::layout')
+    {{-- Header --}}
+    @slot('header')
+        @component('mail::header', ['url' => config('app.url'), 'title' => 'Soemthing Veirficto'])
+            WeeDoo-Documents
+        @endcomponent
+    @endslot
+{{-- Body --}}
+
+  @slot('greeting')
+    Hello {{ $user }},
+  @endslot
+
+  @slot('otherMessage')
+    Please verify your WeeDoo-Documents account by clicking the button below
+  @endslot
+
+        @slot('bodycontents')
+            @component('mail::button', ['url' => $url, 'slot' => $title])
+                Account Verification
+            @endcomponent
+
+            Please ignore the message, if you didnt make the request.
+        @endslot
+
+{{-- Subcopy --}}
+    @isset($subcopy)
+        @slot('subcopy')
+            @component('mail::subcopy')
+               Yours, WeeDoo-Documents Team.
+                {{ $subcopy }}
+            @endcomponent
+        @endslot
+    @endisset
+{{-- Footer --}}
+    @slot('footer')
+        @component('mail::footer')
+            © {{ date('Y') }} WeeDoo-Documents. All rights reserved.
+        @endcomponent
+    @endslot
+@endcomponent
